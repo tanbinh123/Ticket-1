ba65a3c71d09fd4a7f0c7aa8c27a8877269f25d0
ll_5216 <kyokomywife@gmail.com>
2021-02-28 22:09:36 +0800

    annotation fix

--- order/src/main/java/com/woniuxy/order/controller/OrderController.java
+++ order/src/main/java/com/woniuxy/order/controller/OrderController.java
@@ -14,6 +14,8 @@ import com.woniuxy.order.service.OrderService;
 import com.woniuxy.order.service.ScheduleService;
 import com.woniuxy.order.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -66,11 +68,24 @@ public class OrderController {
 //        return "order-add";
 //    }
 
+    /**
+     * 添加订单
+     *
+     * @param userId     用户ID
+     * @param scheduleId 排片ID
+     * @param seatId     座位ID
+     * @return 添加结果
+     */
     @ApiOperation("添加订单")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "用户ID", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "scheduleId", value = "排片ID", required = true, paramType = "query"),
+            @ApiImplicitParam(name = "seatId", value = "座位ID", required = true, paramType = "query")
+    })
     @PostMapping("/add")
-    private ResponseResult<?> add(@Min(value = 1,message = "用户ID应大于{value}") Integer userId,
-                                  @Min(value = 1,message = "排片ID应大于{value}") Integer scheduleId,
-                                  @Min(value = 1,message = "座位ID应大于{value}") Integer seatId) {
+    private ResponseResult<?> add(@Min(value = 1, message = "用户ID应大于{value}") Integer userId,
+                                  @Min(value = 1, message = "排片ID应大于{value}") Integer scheduleId,
+                                  @Min(value = 1, message = "座位ID应大于{value}") Integer seatId) {
         scheduleService.updateSeatStatus("0", scheduleId, seatId);
         userService.updateIntegration(2, userId);
 
@@ -80,7 +95,7 @@ public class OrderController {
         LocalDateTime orderTime = LocalDateTime.now();
         LocalDateTime payTime = LocalDateTime.now();
 
-        String orderNo = orderTime.toString().substring(10).replaceAll("-","") +
+        String orderNo = orderTime.toString().substring(10).replaceAll("-", "") +
                 RandomUtil.randomNumbers(10);
 
         boolean save = orderService.save(new Order().setOrderNo(orderNo).setOrderTime(orderTime).
@@ -90,7 +105,20 @@ public class OrderController {
         return save ? new ResponseResult<>(StateEnum.SUCCESS) : new ResponseResult<>(StateEnum.FAIL);
     }
 
-    @ApiOperation("查询订单")
+    /**
+     * 多条件查询订单
+     *
+     * @param orderNo   订单编号
+     * @param startDate 开始时间
+     * @param endDate   结束时间
+     * @return 订单分页结果
+     */
+    @ApiOperation("多条件查询订单")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "orderNo", value = "订单编号", paramType = "query"),
+            @ApiImplicitParam(name = "startDate", value = "开始时间", paramType = "query"),
+            @ApiImplicitParam(name = "endDate", value = "结束时间", paramType = "query")
+    })
     @GetMapping("/get")
     private ResponseResult<?> getByNoOrTime(String orderNo,
                                             @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -100,16 +128,19 @@ public class OrderController {
                                             @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
                                                     LocalDateTime endDate) {
         QueryWrapper<Order> wrapper = new QueryWrapper<>();
-        if (orderNo != null && !orderNo.trim().equals("")) {
+        // TODO 获取用户ID加入到wrapper条件
+
+        // 条件存在 订单编号
+        if (orderNo != null && !orderNo.trim().equals(""))
             wrapper.eq("order_no", orderNo);
-        }
-        if (startDate != null && endDate != null) {
+        // 条件存在 起止时间
+        if (startDate != null && endDate != null)
             wrapper.between("order_time", startDate, endDate);
-        }
+
         Page<Order> page = new Page<>(1, 2);
         Page<Order> orderPage = orderService.page(page, wrapper);
-        return new ResponseResult<>(orderPage);
 
+        return new ResponseResult<>(orderPage);
     }
 
 }
